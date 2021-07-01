ec208270afbd8f015084dd79907aa6221ae845fb
Ha Manh Do <hwebz.com@gmail.com>
2021-07-01 14:35:49 +0700

    Using REST for resourceful routes in a Rails project

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,29 @@
 Rails.application.routes.draw do
 
-  get 'pages/index'
-  get 'pages/show'
-  get 'pages/new'
-  get 'pages/edit'
-  get 'pages/delete'
-
-  get 'subjects/index'
-  get 'subjects/show'
-  get 'subjects/new'
-  get 'subjects/edit'
-  get 'subjects/delete'
-  
   root 'demo#index'
+
+  # get 'pages/index'
+  # get 'pages/show'
+  # get 'pages/new'
+  # get 'pages/edit'
+  # get 'pages/delete'
+
+  # get 'subjects/index'
+  # get 'subjects/show'
+  # get 'subjects/new'
+  # get 'subjects/edit'
+  # get 'subjects/delete'
+
+  resources :subjects do
+    member do
+      get :delete
+    end
+  end
+  resources :pages do
+    member do
+      get :delete
+    end
+  end
 
   get 'demo/index'
   get 'demo/hello'
